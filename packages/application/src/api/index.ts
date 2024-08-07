3d2e7adb28b5a1f5990abf6f8096beb82f7841a7
asynctomatic <afonso@threesigma.xyz>
2024-08-07 16:18:10 +0100

    feat: fix actions

--- packages/application/src/api/index.ts
+++ packages/application/src/api/index.ts
@@ -2,30 +2,37 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import "reflect-metadata";
-import '@src/api/http/controllers/index.js';
+import "@src/api/http/controllers/index.js";
 
 import { Application, urlencoded, json } from "express";
 
 import config from "@src/config";
 import { TYPES } from "@src/types";
 import { initialize } from "@src/startup";
-import { IEventBus } from "@filecoin-plus/core";
+import { IEventBus, Logger } from "@filecoin-plus/core";
 import { RabbitMQEventBus } from "@src/infrastructure/event-bus/rabbitmq-event-bus";
 import { InversifyExpressServer } from "inversify-express-utils";
 import { errorHandler } from "./http/middlewares/error-handler";
 import { corsMiddleware } from "./http/middlewares/cors-middleware";
+import {
+  subscribeApplicationSubmissions,
+  subscribeGovernanceReviews,
+  subscribeRkhApprovals,
+  subscribeRkhProposals,
+} from "@src/worker";
 
-
-(async () => {
+async function main() {
   // Initialize the container
   const container = await initialize();
+
+  // Get the logger from the container
+  const logger = container.get<Logger>(TYPES.Logger);
 
   // Initialize and configure the API server
   const server = new InversifyExpressServer(container);
   server.setConfig((app: Application) => {
     app.use(urlencoded({ extended: true }));
     app.use(json());
-    
     app.use(corsMiddleware);
   });
   server.setErrorConfig((app: Application) => {
@@ -33,21 +40,37 @@ import { corsMiddleware } from "./http/middlewares/cors-middleware";
   });
 
   // Bind the API server to the container
-  // TODO: Is this necessary?
   const apiServer = server.build();
   container.bind<Application>(TYPES.ApiServer).toConstantValue(apiServer);
 
-  // Initialize RabbitMQ
+  // Initialize RabbitMQ as subscribe to events
   const eventBus = container.get<IEventBus>(TYPES.EventBus) as RabbitMQEventBus;
-  await eventBus.init();
+  try {
+    await eventBus.init();
+    await eventBus.subscribeEvents();
+    logger.info("RabbitMQ initialized successfully");
+  } catch (error) {
+    logger.error("Failed to initialize RabbitMQ", { error });
+    process.exit(1);
+  }
 
-  // Subscribe to events
-  await eventBus.subscribeEvents();
+  // Start worker services
+  // TODO: Move this to application startup
+  await subscribeApplicationSubmissions(container);
+  await subscribeGovernanceReviews(container);
+  // await subscribeRkhProposals(container);
+  // await subscribeRkhApprovals(container);
 
+  // Start the API server
   apiServer.listen(config.API_PORT, () =>
     console.log(
       "The application is initialised on the port %s",
       config.API_PORT
     )
   );
-})();
+}
+
+main().catch((error) => {
+  console.error("Unhandled error while starting the application:", error);
+  process.exit(1);
+});
--- packages/application/src/worker/index.ts
+++ packages/application/src/worker/index.ts
@@ -1,41 +1,4 @@
-import "reflect-metadata";
-
-import { ICommandBus, IEventBus, Logger } from "@filecoin-plus/core";
-
-import { RabbitMQEventBus } from "@src/infrastructure/event-bus/rabbitmq-event-bus";
-import { initialize } from "@src/startup";
-import { TYPES } from "@src/types";
-
-import { subscribeApplicationSubmissions } from "./subscribe-application-submissions";
-import { subscribeGovernanceReviews } from "./subscribe-governance-reviews";
-import { subscribeRkhProposals } from "./subscribe-rkh-proposals";
-import { subscribeRkhApprovals } from "./subscribe-rkh-approvals";
-
-
-async function main() {
-  const container = await initialize();
-
-  // Get the logger from the container
-  const logger = container.get<Logger>(TYPES.Logger);
-
-  // Initialize RabbitMQ
-  const eventBus = container.get<IEventBus>(TYPES.EventBus) as RabbitMQEventBus;
-  try {
-    await eventBus.init();
-    logger.info("RabbitMQ initialized successfully");
-  } catch (error) {
-    logger.error("Failed to initialize RabbitMQ", { error });
-    process.exit(1);
-  }
-
-  // Start the subscriptions
-  await subscribeApplicationSubmissions(container);
-  // await subscribeGovernanceReviews(container);
-  // await subscribeRkhProposals(container);
-  // await subscribeRkhApprovals(container);
-}
-
-main().catch((error) => {
-  console.error("Unhandled error in worker:", error);
-  process.exit(1);
-});
+export { subscribeApplicationSubmissions } from "./subscribe-application-submissions";
+export { subscribeGovernanceReviews } from "./subscribe-governance-reviews";
+export { subscribeRkhProposals } from "./subscribe-rkh-proposals";
+export { subscribeRkhApprovals } from "./subscribe-rkh-approvals";
