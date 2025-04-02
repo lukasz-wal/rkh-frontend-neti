844ae53937d3cac0e3c358009f4824624b14e299
Will <will@fidl.tech>
2025-04-02 14:11:45 +0000

    Update message.service.ts

--- packages/application/src/application/services/message.service.ts
+++ packages/application/src/application/services/message.service.ts
@@ -96,7 +96,7 @@ export class MessageService {
   private getKYCStatusMessage(application: DatacapAllocator): string {
     return `
 ### Next Steps
-1. Complete the KYC process at [our secure portal](https://flow.togggle.io/fidl/kyc?applicationId=${application.guid})
+1. Complete the KYC process at [our secure portal](https://flow.togggle.io/fil/kyc?applicationId=${application.guid})
    - **Name:** ${application.applicantName}
    - **GitHub Username:** ${application.applicantGithubHandle}
 2. Your application will be automatically updated once submitted
